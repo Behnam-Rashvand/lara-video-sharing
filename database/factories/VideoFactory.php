16ce0a121d5beeeb0a0080f974012e024d46849e
Behnam.R <Behnam.rashvand.17@gmail.com>
2023-12-01 00:49:48 +0330

    create video factory and rename model video

---
+++ database/factories/VideoFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Video>
+ */
+class VideoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'=> fake()->name(),
+            'slug'=> fake()->slug(),
+            'url' => fake()->imageUrl(640,480,'animals' , true),
+            'length' => fake()->randomNumber(3 , false) ,
+            'description' => fake()->sentence(rand(2,5)),
+        ];
+    }
+}
