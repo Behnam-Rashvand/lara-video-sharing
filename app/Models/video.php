6b74a6bbe054e1ed422a3f0df637b042e2322f8e
Behnam.R <Behnam.rashvand.17@gmail.com>
2023-12-04 16:19:06 +0330

    create accessors in model video and add persian date by verta

--- app/Models/video.php
+++ app/Models/video.php
@@ -2,10 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Video extends Model
 {
     use HasFactory;
+
+
+    public function length(): Attribute
+    {
+        return Attribute::make(
+            get: fn ( int $value ) => gmdate('H:i',$value) ,
+        );
+    }
+
+    public function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => verta($value)->formatDifference(),
+        );
+    }
 }
