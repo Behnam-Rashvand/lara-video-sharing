cdd7293623e2060376e2345aefd79e72d1968b7a
Behnam.R <Behnam.rashvand.17@gmail.com>
2023-12-17 22:29:07 +0330

    ref: custom register page in auth.register

---
+++ resources/views/auth-layout.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Video Post – Video Sharing HTML Template</title>
+    <meta name="keywords" content="Blog website templates" />
+    <meta name="description" content="Author - Personal Blog Wordpress Template">
+    <meta name="author" content="Rabie Elkheir">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap Core CSS -->
+    <!-- Owl Carousel Assets -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+        type="text/css" />
+
+    <!--Google Fonts-->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800|Raleway:400,500,700|Roboto:300,400,500,700,900|Ubuntu:300,300i,400,400i,500,500i,700"
+        rel="stylesheet">
+    <!-- Main CSS -->
+    @vite('resources/css/app.css')
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+</head>
+
+<body class="@yield('class-body')">
+
+    @if( session('alert') )
+    <div class="alert alert-success">
+        {{ session('alert') }}
+    </div>
+    @endif
+    
+    @yield('content')
+
+    @vite('resources/js/app.js')
+</body>
+
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,57 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+@extends('auth-layout')
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+@section('class-body' , 'sing-up-page')
+
+@section('content')
+    <!--======= log_in_page =======-->
+    <div id="log-in" class="site-form log-in-form">
+
+        <div id="log-in-head">
+            <h1>ثبت نام</h1>
+            <div id="logo">
+                <a href="{{ route('home') }}">
+                    <img src="{{ Vite::image('logo.png') }}" alt="">
+                </a>
+            </div>
         </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div class="form-output">
+            <x-validation-error />
+            <form action="{{ route('register.store') }}" method="POST">
+                @csrf
+                <div class="form-group label-floating">
+                    <label class="control-label">نام</label>
+                    <input name="name" class="form-control" placeholder="" type="text">
+                </div>
+                <div class="form-group label-floating">
+                    <label class="control-label">ایمیل</label>
+                    <input name="email" class="form-control" placeholder="" type="email">
+                </div>
+                <div class="form-group label-floating">
+                    <label class="control-label">رمز عبور</label>
+                    <input name="password" class="form-control" placeholder="" type="password">
+                </div>
+
+                <div class="form-group label-floating">
+                    <label class="control-label">تأیید رمز عبور</label>
+                    <input name="password_confirmation" class="form-control" placeholder="" type="password">
+                </div>
+
+                <div class="remember">
+                    <div class="checkbox">
+                        <label>
+                            <input name="remember" type="checkbox">
+                            مرا به خاطر بسپار
+                        </label>
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-lg btn-primary full-width">ثبت نام</button>
+
+                <div class="or"></div>
+
+                <p>شما یک حساب کاربری دارید؟ <a href="{{ route('login.create') }}"> ورود!</a> </p>
+            </form>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+@endsection
