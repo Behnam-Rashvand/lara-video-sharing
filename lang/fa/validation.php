150a34454013580a2bd57bcf0adfa135d6336bf1
Behnam.R <Behnam.rashvand.17@gmail.com>
2023-12-17 16:23:53 +0330

    fix: fix error in validation lang

--- lang/fa/validation.php
+++ lang/fa/validation.php
@@ -142,11 +142,12 @@ return [
         'slug' => 'نام یکتا',
         'url' => 'آدرس ویدیو',
         'thumbnail' => 'تصویر بندانگشتی',
-        'password' => [
-            'current_password' => 'رمز عبور کنونی',
-            'password' => 'رمز عبور',
-            'password_confirmation' => 'تایید رمز عبور'
-        ],
+        // 'password' => [
+        //     'current_password' => 'رمز عبور کنونی',
+        //     'password' => 'رمز عبور',
+        //     'password_confirmation' => 'تایید رمز عبور'
+        // ],
+        'current_password' => 'رمز عبور کنونی',
         'is_special_offer' => 'پیشنهاد ویژه',
         "user" => [
 
