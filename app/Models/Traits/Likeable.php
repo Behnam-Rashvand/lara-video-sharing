a4d5bc923e4e1aa30abadec6c29e195c630277ef
Behnam.R <Behnam.rashvand.17@gmail.com>
2023-12-24 19:05:23 +0330

    chore: display like,dislike in videos.show

--- app/Models/Traits/Likeable.php
+++ app/Models/Traits/Likeable.php
@@ -1,16 +1,35 @@
 <?php
-namespace App\Models\Traits ;
+
+namespace App\Models\Traits;
 
 use App\Models\Like;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
-
-
-trait Likeable {
+trait Likeable
+{
 
 
 
     public function likes()
     {
-        return $this->morphMany(Like::class , 'likeable');
+        return $this->morphMany(Like::class, 'likeable');
+    }
+
+    public function likesCount(): Attribute
+    {
+        return (Attribute::make(
+            fn () => $this->likes()
+                ->where('vote', 1)
+                ->count()
+        ));
+    }
+
+    public function dislikesCount(): Attribute
+    {
+        return (Attribute::make(
+            fn () => $this->likes()
+            ->where('vote' , -1)
+            ->count()
+        ));
     }
 }
--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -18,8 +18,14 @@
 
                 <div class="video-share">
                     <ul class="like">
-                        <li><a class="deslike" href="#">1250 <i class="fa fa-thumbs-down"></i></a></li>
-                        <li><a class="like" href="{{ route('videos.like' , $video) }}">1250 <i class="fa fa-thumbs-up"></i></a></li>
+                        <li><a class="deslike" href="#">
+                            {{ $video->dislikes_count }}
+                             <i class="fa fa-thumbs-down"></i>
+                            </a></li>
+                        <li><a class="like" href="{{ route('videos.like' , $video) }}">
+                            {{ $video->likes_count }}
+                             <i class="fa fa-thumbs-up"></i>
+                            </a></li>
                     </ul>
                     <ul class="social_link">
                         <li><a class="facebook" href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
